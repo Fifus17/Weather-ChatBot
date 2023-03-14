54b0c94f9efe717bc3bbbf062599e4169d43633a
Fifus17 <filipdziurdzia2002@gmail.com>
2023-03-14 09:22:46 +0100

    message input component done

---
+++ Frontend/src/Components/Chat.js
@@ -0,0 +1,16 @@
+
+import MessageInput from "./MessageInput";
+import "./Chat.css"
+
+const Chat = () => {
+
+    return (
+        <div className="chat-container">
+            <div className="chat-wrapper">
+            <MessageInput/>
+            </div>
+        </div>
+    )
+}
+
+export default Chat;
