98f00eb9b0c019442a84ce75d71f212e0824b0a4
Neashe <nat.adamiak@gmail.com>
2023-03-26 16:54:16 +0200

    some weather fetching functions updated (still in progress)

--- Backend/chatbot/weather_fetch.py
+++ Backend/chatbot/weather_fetch.py
@@ -1,11 +1,26 @@
 """set of functions needed to fetch the weather from API"""
-import requests,json
-#my API key DO UZUPELNIENIA
-api_key = ""
-base_url = ""
+import datetime
+import time
 
+import requests,json
+
+api_key = "1b311ee45be9d517454cab7c6dc3b9f5"
+base_url = "https://api.openweathermap.org/data/2.5/weather?"
+base_url_geo = "http://api.openweathermap.org/geo/1.0/direct?"
+base_url_onecall = "https://api.openweathermap.org/data/2.5/onecall?"
+def kelvin_to_celcius(temp):
+    return temp-273.15
+def get_location_from_city(city_name):
+    complete_url = base_url_geo+"&q="+city_name+"&limit=1"+"&appid="+api_key
+    response = requests.get(complete_url).json()
+    if response:
+        return response[0]["lat"],response[0]["lon"]
+    return None
+
+print(get_location_from_city("London"))
+"""Funkcja używa weather API i printuje aktualną temperaturę dla danego miasta"""
 def get_currentWeather(city_name):
-    complete_url = base_url+"appid="+api_key+"&q="+city_name
+    complete_url = base_url+"appid="+api_key+"&q="+city_name+"&limit=1"
 
     response = requests.get(complete_url)
     x = response.json()
@@ -13,6 +28,28 @@ def get_currentWeather(city_name):
         y = x["main"]
         current_temperature = y["temp"]
         #here we can get more things to describe weather
-        print("Current temperature at "+city_name,": ",current_temperature)
+        print("Current temperature at "+city_name,": ",round((kelvin_to_celcius(current_temperature)),2),"(in celsius)")
     else:
         print("City not found")
+
+"""funkcja przyjmuje date (rok, miesiąc, dzień) i zwraca date w formacie UNIX timestamp (defaultowo dla
+godziny 12"""
+def get_unixTime(year,month,day,hour=12):
+    unix_time = datetime.datetime(year,month,day,hour)
+    return time.mktime(unix_time.timetuple())
+
+"""Funkcja przyjmuje nazwe miasta i zwraca aktualną pogodę (wersja 2: na podany dzień i godzinę)"""
+
+def get_weather(city_name):
+    lat,lon = get_location_from_city(city_name) #pobieramy współrzędne dla danego miasta
+    complete_url = base_url_onecall+"lat="+str(lat)+"&lon="+str(lon)+"&exclude=minutely,hourly"+"&appid="+api_key
+    response = requests.get(complete_url).json()
+    print(response)
+    print(f"Current temperature in {city_name}: ",kelvin_to_celcius(response["current"]["temp"]))
+    # print(f"Weather description: {response['current']['weather']['description']}")
+
+get_currentWeather("London")
+get_weather("London")
+
+
+
