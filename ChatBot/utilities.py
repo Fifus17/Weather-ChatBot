904b981864f6f35a570d1b01bb83f0a66c25ff58
Fifus17 <filipdziurdzia2002@gmail.com>
2023-03-26 22:12:55 +0200

    utility functions done

---
+++ ChatBot/utilities.py
@@ -0,0 +1,40 @@
+# For the first run
+# nltk.download('punkt')
+
+# Imports
+import numpy as np
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+# Initializing the stemmer
+stemmer = PorterStemmer()
+
+# Function for splitting sentences into individual words and interpunction characters
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+# Stemming function - cutting of the sufixes
+def stem(word):
+    return stemmer.stem(word.lower())
+
+# Creating bag of words, returns array of 0's and 1's depending on occurance of word in know words
+def bagOfWords(sentence, allWords):
+
+    # Stemming
+    givenWords = [stem(word) for word in sentence]
+
+    bag = np.zeros(len(allWords), dtype=np.float32)
+    for idx, word in enumerate(allWords):
+        if word in givenWords:
+            bag[idx] = 1
+
+    return bag
+
+# Testing
+tokenizedSentence = tokenize("create creator creating universe universal")
+print(tokenizedSentence)
+
+print([stem(word) for word in tokenizedSentence])
+
+allWords = ['creat', 'home', 'you', 'not', 'univers', 'you']
+print(bagOfWords(tokenizedSentence, allWords))
